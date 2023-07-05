3e7da1f8b206630966bcff06994e82b1da0e13f2
Piotr Nagórny <68472499+pnagorny@users.noreply.github.com>
2023-07-05 08:52:16 +0200

    Password Length fix
    Fixed the length of the password to always be 8 chars

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from os.path import exists
-import os
-import random
 import secrets
 import string
 
@@ -15,18 +13,19 @@ def generatePass():
     
     pwd = ""
     while True:
-        for _ in range(pwd_length):
+        for x in range(pwd_length):
             pwd += "".join(secrets.choice(alphabet))
             
         if (any(char in special_chars for char in pwd) 
             and any(char in digits for char in pwd)>=1
             and any(char in lettersU for char in pwd)>=1
             and any(char in lettersL for char in pwd)>=1):
+            f = open("passwords.txt", "w")
+            f.write(pwd)
+            f.close()
             break
-    return pwd
+        pwd = ""
 
 def main():
-    f = open("passwords.txt", "w")
-    f.write(generatePass())
-    f.close()
+    generatePass()
 main()
